9c8d9d303d7d32663d359490bb9c04cbe1ea957e
Turawa Abdur Rahman <rahmaniy2002@gmail.com>
2018-10-03 23:38:52 +0100

    wrote the result of console log in a coment block

--- convertTobase2.js
+++ convertTobase2.js
@@ -10,19 +10,19 @@ function base2(number){
 }
 
 
-console.log(base2(8));
-console.log(base2(1));
-console.log(base2(2));
-console.log(base2(3));
-console.log(base2(4));
-console.log(base2(5));
-console.log(base2(6));
-console.log(base2(7));
-console.log(base2(9));
-console.log(base2(10));
-console.log(base2(11));
-console.log(base2(12));
-console.log(base2(13));
-console.log(base2(14));
-console.log(base2(15));
-console.log(base2(16));
+console.log(base2(1));  // 1
+console.log(base2(2));  // 10
+console.log(base2(3));  // 11
+console.log(base2(4));  // 100
+console.log(base2(5));  // 101
+console.log(base2(6));  // 110
+console.log(base2(7));  // 111
+console.log(base2(8));  // 1000
+console.log(base2(9));  // 1001
+console.log(base2(10)); // 1010
+console.log(base2(11)); // 1011
+console.log(base2(12)); // 1100
+console.log(base2(13)); // 1101
+console.log(base2(14)); // 1110
+console.log(base2(15)); // 1111
+console.log(base2(16)); // 10000
